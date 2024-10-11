26aeccc2ccf2cd2d25688c9e380f37f38b310fd2
Samuel Leat <samuel.leat@nhs.net>
2024-10-11 12:01:05 +0100

    Updated the FAQ to reflect the new tool appearance

--- pages/FAQs.py
+++ pages/FAQs.py
@@ -80,6 +80,10 @@ with st.expander("What does the tool do?"):
     are deemed to be avoidable at all ages.""")
 
 with st.expander("How do I create a place?"):
+    st.markdown("""
+            To create a place, input is required on the left-hand side of the tool. Please note that the Default place that is shown when first accessing the site, is for illustration only, it will not be included in the download under detailed in *How do I download my saved places data?* below. If you do want to keep this place in the download, please rename it.
+            """)
+
     col1, col2 = st.columns(2)
 
     with col1:
@@ -87,8 +91,7 @@ with st.expander("How do I create a place?"):
 
     with col2:
         st.markdown("""
-            To create a place, input is required on the left-hand side of the tool. Please note that the Default place that is shown when first accessing the site, is for illustration only, it will not be included in the download under Step 3 in Section 4. If you do want to keep this place in the download, please rename it.
-            \n\n**1a:** Unlike the previous version of this tool, this version covers two years (2023/24 and 2024/25). Please select which year you would like to tool to show by selecting this in the dropdown indicated in page element 1a). Please note that the download function (steps 3a-3c) will only download the selected year.
+            **1a:** Unlike the previous version of this tool, this version covers two years (2023/24 and 2024/25). Please select which year you would like to tool to show by selecting this in the dropdown indicated in page element 1a). Please note that the download function (steps 3a-3c) will only download the selected year.
             \n\n**1b:** Select the ICB of interest. You can select only one ICB at a time. This will filter the options available to you in the two drop-down menus below (Local Authority District Filter and Select GP Practice).
             \n\n**1c:** If you want to select GP practices from one or more specific Local Authority District(s), select these in this dropdown. You can select multiple Local Authority Districts at once.
             """)
@@ -101,11 +104,11 @@ with st.expander("How do I create a place?"):
     st.markdown("""
         - If you want to make changes to a place you already defined you can add more GP practices in 1d and press “Save Place” again to update.
         - Once you have saved a place, you can then create a new place in the same way. You will not overwrite the place you created previously as long as you have saved it under a new name.
-        - If you want to delete a place and start again, please refer to Step 2 under Section 3.
+        - If you want to delete a place and start again, please refer to Step 2 under *How do I view my saved places?* below.
         - You can select the same GP practice to be in different places (e.g. if you create place A and B, both can include GP practice X).
         """)
     st.markdown("""
-        **Warning:** Refreshing the web page will reset all your inputs including your saved places. Only refresh the page if you do not want to keep your saved places. Alternatively, download your work first (under Section 4 / Step 3) which will download a save file for the tool from which you can return to your saved places.
+        **Warning:** Refreshing the web page will reset all your inputs including your saved places. Only refresh the page if you do not want to keep your saved places. Alternatively, download your work first (under Step 3 of *How do I download my saved places data?* below) which will download a save file for the tool from which you can return to your saved places.
         """)
 
 with st.expander("How do I view my saved places?"):
@@ -118,8 +121,9 @@ with st.expander("How do I view my saved places?"):
         **2a)** Once you have created a new place, the place name will become available in the main page dropdown menu. If you create multiple places, you can switch between them here. The place you select in this dropdown is the active place, and while it is active (i.e. you have it selected) the page elements 2b, 2c, 2d, and 2e will all refer to the active place.
         \n\n**2b)** If you want to remove the active place completely, click the button “Delete current selection”. If you delete all of your saved places, the app will return to the default place shown in Figure 1.
         \n\n**2c)** The map feature provides a helpful check on the geography and relative locations of the selected GP practices. This is useful to check that you included those GP practices in your defined place that you intended.
-        \n\n**2d)** This box lists the GP practices that make up the active year and place you selected in steps 1a and 2a. This information is here to also help you check whether you have the desired GP practices in your defined place.
-        \n\n**2e)** The dashboard provides the relative need indices for the active year and place you selected under steps 1a and 2a. These are the combined need indices for the GP practices that make up the active place selected. These need indices are always relative to the ICB (i) and defined place (p) given by the formula:
+        \n\n**2d)** This box identifies the time period for the data being displayed.
+        \n\n**2e)** This box lists the GP practices that make up the active year and place you selected in steps 1a and 2a. This information is here to also help you check whether you have the desired GP practices in your defined place.
+        \n\n**2f)** The dashboard provides the relative need indices for the active year and place you selected under steps 1a and 2a. These are the combined need indices for the GP practices that make up the active place selected. These need indices are always relative to the ICB (i) and defined place (p) given by the formula:
         """)
     st.latex(r'''
         {WP_{p}/GP_{p} \over WP_{i}/GP_{i}}
@@ -141,7 +145,7 @@ with st.expander("How do I download my saved places data?"):
     """)
     st.markdown("""
     - 'ICB allocation calculations.csv': The data you previewed under step 3b in a Comma Separated Value (.csv) file which can be opened as a table in Microsoft Excel.
-    - 'ICB allocation tool configuration file.json': A JavaScript Object Notation (JSON) file which can be used to re-upload your saved places into the tool at another time and return to the session you just downloaded. This is useful if you have defined many different places and want to come back to these places without having to redefine them. More information on this can be found in Step 4 in Section 5 of this guide.
+    - 'ICB allocation tool configuration file.json': A JavaScript Object Notation (JSON) file which can be used to re-upload your saved places into the tool at another time and return to the session you just downloaded. This is useful if you have defined many different places and want to come back to these places without having to redefine them. More information on this can be found in Step 4 of *How do I save and return to my session?*, below.
     - 'ICB allocation tool documentation.txt': A plain text file with reference information on the AIF Place Based Tool, including a link to NHS England’s GitHub<sup>1</sup> repository from which the tool runs on the Streamlit<sup>2</sup> app. The GitHub repository provides further technical information on the tool.
     """, unsafe_allow_html=True)
     st.markdown("""
@@ -155,6 +159,7 @@ with st.expander("How do I download my saved places data?"):
 with st.expander("How do I save and return to my session?"):
     st.markdown("""
     To be able to return to your session you will need the .json file described under step 3c of the “How do I download my saved places data?” section above. Alternatively, you are also able to download the .json file without downloading the full .zip, as detailed in step 4b below.
+    \n\nYou can return to a previous session for which you downloaded result using the .json file in the .zip you downloaded, as follows:
     """)
     col1, col2 = st.columns(2)
 
@@ -163,11 +168,13 @@ with st.expander("How do I save and return to my session?"):
 
     with col2:
         st.markdown("""
-            You can return to a previous session for which you downloaded result using the .json file in the .zip you downloaded, as follows:
-            \n\n**4a)** On the left-hand side of the tool, below the “Save Place” button, tick “Advanced Options”.
+            **4a)** On the left-hand side of the tool, below the “Save Place” button, tick “Advanced Options”.
             \n\n**4b)** The new menu that appears and has a button called “Download session data as JSON”. Click this to download just the .json save file to return to later, if you don’t want to download the full .zip file.
             \n\n**4c)** From the menu that appears, click “Browse files” and use the new window to find the file location of the .json file from a previous session that you would like to reload in the tool. Alternatively, simply drag and drop the .json save file where the tool indicates “Drag and drop file here”.
-            \n\nClick “Submit” to reload the session. You should now see the saved places in the selection box at the top of the page. You can now add or delete any of your saved places if you wish.
+            """)
+    
+    st.markdown("""
+            Click “Submit” to reload the session. You should now see the saved places in the selection box at the top of the page. You can now add or delete any of your saved places if you wish.
             \n\n**4d)** You can also see the current session data (that will be downloaded in step 4b by clicking the “Show Session Data” button under the Advanced Options. This session data will then be printed out at the bottom of the main page as long as this check box is ticked.
             """)
 
